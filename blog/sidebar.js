8b4b41522dcba76f8a3a78afd0d8a0a2316f6619
interestingLSY <2659723130@qq.com>
2018-10-09 11:54:18 +0800

    Add Contact

--- blog/sidebar.js
+++ blog/sidebar.js
@@ -1,17 +1,24 @@
 function Bottom(){
 document.write(" \
 	<section class=\"card\" onclick=\"Top();\"> \
-		<p class=\"describe\" style=\"font-size:15px\"> 没东西啦！再看一遍吧qwq！点击来回到顶部！=w= </p> \
+		<p class=\"describe\" style=\"font-size:15px\"> 没东西啦！再看一遍吧qwq！点击这里来回到顶部！=w= <br /> \
+		顺便说一句，点击每页最上方的大标题可以返回上一页qwq！</p> \
 	</section> \
 	<a href=\"../../../about.html\"> \
 		<section class=\"card\"> \
 			<p class=\"proud\"> 自豪地使用自己亲手写的blog </p> \
 		</section> \
 	</a> \
-");
+")
+}
+
+function Up(){
+	if( document.documentElement.scrollTop == 0 ) return;
+	document.documentElement.scrollTop -= document.documentElement.scrollTop/40.0;
+	t = setTimeout(function(){Up()},5);
 }
 function Top(){
-	document.documentElement.scrollTop = document.body.scrollTop = -22200;
+	Up()
 }
 function Sidebar(){
 document.write(" \
@@ -19,10 +26,10 @@ document.write(" \
 	<section class=\"sidecard\"> \
 		<img src=../../../img/index/head.jpeg class=\"head\"> </img> \
 		<h1 class=\"sidetitle\"> intlsy </h1> \
-		<h2 class=\"sidetitle\"> 诗酒趁年华 </h2> \
+		<h2 class=\"sentence\"> 诗酒趁年华 </h2> \
 	</section> \
 	<section class=\"sidecard\"> \
-		<h2 class=\"sidetitle\"> dalao们 </h1> \
+		<h2 class=\"sidetitle\"> dalao们 </h2> \
 		<a href=\"https://captain1.cnblogs.com/\"> \
 			<div class=\"sidecont\"> \
 				<div style=\"color:#000;display:inline;\"> L</div><div style=\"color:#f00;display:inline;\">iboao </div>大佬的博客 \
@@ -38,7 +45,27 @@ document.write(" \
 				BZY 红胖子 (bin哥) \
 			</div> \
 		</a> \
+		<a href=\"http://www.cnblogs.com/ivorysi\"> \
+			<div class=\"sidecont\"> \
+				在下司公子，又名4 + 7 \
+			</div> \
+		</a> \
+		<a href=\"https://shulker.cnblogs.com/\"> \
+			<div class=\"sidecont\"> \
+				.....(nxy的博客) \
+			</div> \
+		</a>  \
+	</section> \
+	<section class=\"sidecard\"> \
+		<h2 class=\"sidetitle\"> 试着联系我？ </h2> \
+		<div class=\"sidecont\"> \
+			qq: 2659723130 <br /> \
+			luogu: <a href=\"https://www.luogu.org/space/show?uid=25630\">interestingLSY,uid:25630 </a> <br /> \
+			codeforces: <a href=\"http://codeforces.com/profile/interestingLSY\">interestingLSY</a> <br /> \
+			coding.net: <a href=\"https://coding.net/u/interestingLSY/main\">interestingLSY</a> <br /> \
+			博客出锅了记得跟我说qwq \
+		</div> \
 	</section> \
 </div> \
-");
+")
 }
